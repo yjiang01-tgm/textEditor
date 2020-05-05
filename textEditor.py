42776d670c8c07b20f3a16258750c52b9e704604
Yuchen Jiang <yjiang01@student.tgm.ac.at>
2020-05-05 15:58:45 +0200

    Text öffnen, editieren, speichern und löschen

--- textEditor.py
+++ textEditor.py
@@ -7,21 +7,33 @@ from tkinter import filedialog
 
 
 class Editor:
+
+    def __init__(self):
+        self.currentFile = ""
+
     def newF(self):
         Tk().withdraw()
         filename = filedialog.asksaveasfilename(title='Speicherort auswaehlen', filetypes=[('Text', '*.txt')])
         if filename:
             if not filename.endswith('.txt'):
                 filename += '.txt'
+            self.currentFile = filename
             open(filename, "w")
 
     def openF(self):
         Tk().withdraw()
-        filename = filedialog.askopenfilename(title='Speicherort auswaehlen', filetypes=[('Text', '*.txt')])
+        filename = filedialog.askopenfilename(title='Datei auswaehlen', filetypes=[('Text', '*.txt')])
         if filename:
-            print(filename)
+            self.currentFile = filename
+            with open(filename, "r") as file:
+                ui.textEdit.setPlainText(file.read())
+
+    def saveF(self):
+        with open(self.currentFile, "w") as file:
+            file.write(ui.textEdit.toPlainText())
 
     def deleteF(self):
+        os.remove(self.currentFile)
         print("close")
 
     def closeF(self):
@@ -37,6 +49,7 @@ if __name__ == "__main__":
     editor = Editor()
     ui.actionOpen.triggered.connect(editor.openF)
     ui.actionNew.triggered.connect(editor.newF)
+    ui.actionSave.triggered.connect(editor.saveF)
     ui.actionDelete.triggered.connect(editor.deleteF)
     ui.actionClose.triggered.connect(editor.closeF)
 
