b071e3cb9a7be368ada230681dba36def886680e
Yuchen Jiang <yjiang01@student.tgm.ac.at>
2020-05-05 13:01:04 +0200

    Python GUI kann gestartet werden

---
+++ textEditor.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from gui import Ui_MainWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    sys.exit(app.exec_())
