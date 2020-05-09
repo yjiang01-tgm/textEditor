ec37cbebf4d3a98bb7492637f74838c4bb0cc628
Yuchen Jiang <yjiang01@student.tgm.ac.at>
2020-05-09 11:15:58 +0200

    Mehrere Tabe können angezeigt werden

--- textEditor.py
+++ textEditor.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from gui import Ui_MainWindow
 from tkinter import Tk
@@ -10,34 +11,50 @@ class Editor:
 
     def __init__(self):
         self.currentFile = ""
+        self.dict = {}
 
     def newF(self):
         Tk().withdraw()
         filename = filedialog.asksaveasfilename(title='Speicherort auswaehlen', filetypes=[('Text', '*.txt')])
         if filename:
+            self.disableNewDocument()
             if not filename.endswith('.txt'):
                 filename += '.txt'
             self.currentFile = filename
             open(filename, "w")
 
+            self.dict[filename] = QtWidgets.QPushButton(ui.centralwidget)
+            self.dict[filename].setText(filename)
+            self.dict[filename].clicked.connect(lambda: self.changeCurrent(filename))
+            ui.layoutTabs.addWidget(self.dict[filename])
+
+    def changeCurrent(self, filename):
+        self.currentFile = filename
+
     def openF(self):
         Tk().withdraw()
         filename = filedialog.askopenfilename(title='Datei auswaehlen', filetypes=[('Text', '*.txt')])
         if filename:
+            self.disableNewDocument()
             self.currentFile = filename
             with open(filename, "r") as file:
                 ui.textEdit.setPlainText(file.read())
 
     def saveF(self):
+        if self.currentFile == "":
+            self.currentFile = "Neues Dokument.txt"
         with open(self.currentFile, "w") as file:
             file.write(ui.textEdit.toPlainText())
 
     def deleteF(self):
         os.remove(self.currentFile)
-        print("close")
 
     def closeF(self):
         print("close")
+
+    def disableNewDocument(self):
+        if self.currentFile == "":
+            ui.newDocument.setParent(None)
 
 
 if __name__ == "__main__":
